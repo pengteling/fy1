ffbf1e87c7f0ca8aa7e6f2adb0801363ee970c5b
pengteling <pengteling@gmail.com>
2016-05-03 14:21:11 +0800

    tet

---
+++ js/index.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV !== 'production') { //开发环境下 raw-loader  html文件 动态加载
+    require('../index.html');
+}
+;
+//require("./superslide/jquery.SuperSlide.2.1.2.js");
+//require("./../css/style.css");
+require("./../sass/style.scss");
